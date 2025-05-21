e0483165065bfe8866002b8f186f090c369d0d36
Carlos Torres <cedguzman@poligran.edu.co>
2025-05-21 09:36:23 -0500

    feat: Discovery, Product y Booking, funcionales para su consumo a través de Postman

--- payment-microservice/src/main/java/com/edu/poli/payment_microservice/Repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.edu.poli.payment_microservice.Repository;
-
-import com.edu.poli.payment_microservice.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, String> {
-    // Puedes agregar métodos personalizados si los necesitas después
-}
